6f9d90c2e34b7a9251f5fd574c44718361d5c3a1
zhenn <zhenn.life@gmail.com>
2016-04-20 10:19:28 +0800

    ci

--- middleware/swf.js
+++ middleware/swf.js
@@ -6,17 +6,24 @@
 var fs = require('fs'); 
 
 module.exports = function *(next) {
-	var mediaTypes = ['swf'];
+	var mediaTypes = ['swf', 'm3u8', 'ts'];
+	var mimeMap = {
+		swf: 'application/x-shockwave-flash',
+		m3u8: 'application/x-mpegURL',
+		ts: 'video/MP2T'
+	};
 	if (this.file && mediaTypes.indexOf(this.file.type) > -1) {
+		// console.log(this.file.type)
 		var stream = fs.createReadStream(this.localPath, {
 			flags : "r", 
 			encoding : null
 		});
-		this.set('Content-Type' , 'application/x-shockwave-flash');
+		this.set('Content-Type' , mimeMap[this.file.type]);
 		this.set('Accept-Ranges' , 'bytes');
 		this.set('Access-Control-Allow-Origin' , '*');
 		this.body = stream;	
 	}
+
 
 	yield next;
 }
